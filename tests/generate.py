f7b7e848fda6e0a6d8c68b373ccccb792f54e691
Mark <mverleg.noreply@gmail.com>
2024-11-19 23:28:00 +0100

    Generate more of the euler 2 example program

--- tests/generate.py
+++ tests/generate.py
@@ -7,22 +7,14 @@ assert path.exists(gen_pth), f"run codegen to generate {gen_pth}"
 sys.path.insert(0, gen_pth)
 
 from telir.builtin_type_pb2 import BuiltinType
+from telir.builtin_function_pb2 import BuiltinFunc
 from telir.type_pb2 import TypeRef, TypedName
 from telir.struct_pb2 import Struct
 from telir.function_pb2 import Function
+from telir.expression_pb2 import Expression, Read, Store, Call, If, While, Return
 from telir.debug_pb2 import SourceFile
 from telir.tel_pb2 import TelProgram
 
-# def euler(max):
-#     sum = 0
-#     first, second = 1, 1
-#     while second < max:
-#         new = first + second
-#         if new % 2 == 0:
-#             sum += new
-#         first = second
-#         second = new
-#     print(sum)
 prog = TelProgram(
     program_name='euler2',
     ir_version=1,
@@ -40,7 +32,7 @@ def even_fib_sub(max):
             sum += new
         first = second
         second = new
-    print(sum)
+    return sum
 ''',
     )],
     structs=[
@@ -57,17 +49,28 @@ def even_fib_sub(max):
             id=0,
             name="even_fib_sub",
             args=[
-                TypedName(name="max", typ=TypeRef(builtin=BuiltinType.S_INT_64))
+                TypedName(name="max", typ=TypeRef(builtin=BuiltinType.S_INT_64)),  # 0
             ],
             results=[
                 TypeRef(builtin=BuiltinType.S_INT_64)
             ],
             locals=[
-                TypedName(name="sum", typ=TypeRef(builtin=BuiltinType.S_INT_64)),
-                TypedName(name="first", typ=TypeRef(builtin=BuiltinType.S_INT_64)),
-                TypedName(name="second", typ=TypeRef(builtin=BuiltinType.S_INT_64)),
+                TypedName(name="sum", typ=TypeRef(builtin=BuiltinType.S_INT_64)),  # 1
+                TypedName(name="first", typ=TypeRef(builtin=BuiltinType.S_INT_64)),  # 2
+                TypedName(name="second", typ=TypeRef(builtin=BuiltinType.S_INT_64)),  # 3
+                TypedName(name="_1", typ=TypeRef(builtin=BuiltinType.BOOL)),  # 4
             ],
-            code=[],
+            code=[
+                Expression(store=Store(var_ix=1, value=Expression(int=0))),
+                Expression(store=Store(var_ix=2, value=Expression(int=1))),
+                Expression(store=Store(var_ix=3, value=Expression(int=1))),
+                Expression(store=Store(var_ix=4, value=Expression(int=1))),
+                Expression(while_=While(condition_var_ix=4, code=[
+                    Expression(store=Store(var_ix=2, value=Expression(call=Call(builtin=BuiltinFunc.ADD_S64, arguments=[
+                        Expression(read=Read(var_ix=2)), Expression(read=Read(var_ix=3))])))),
+                ])),
+                Expression(return_=Return(var_ix=1)),
+            ],
         ),
     ],
 )

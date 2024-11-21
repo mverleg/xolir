a99b0ea5baf8b97faf7b6b87a807ae1f44e50ba1
Mark <mverleg.noreply@gmail.com>
2024-11-21 22:15:47 +0100

    More input and output java code

--- tests/generate_java/src/main/java/telir_generate_java/GenerateEuler2Java.java
+++ tests/generate_java/src/main/java/telir_generate_java/GenerateEuler2Java.java
@@ -9,23 +9,43 @@ import java.nio.file.Path;
 
 public class GenerateEuler2Java {
     public static void main(String[] args) {
+        var inputPath = getInputPath(args);
+        var tel = readTel(inputPath);
+        var outputPath = buildOutputPath(inputPath, tel);
+        compileToJava(tel, outputPath);
+        System.out.println(tel);
+    }
+
+    private static Path getInputPath(String[] args) {
         if (args.length != 1) {
             System.err.println("First argument should be the path to Telir binary proto file");
             System.exit(1);
         }
-        TelProgram tel = readTel(args);
-        System.out.println(tel);
+        return FileSystems.getDefault().getPath(args[0]);
     }
 
-    private static TelProgram readTel(String[] args) {
+    private static Path buildOutputPath(Path inputPath, TelProgram tel) {
+        var dirPath = Path.of(inputPath.getParent().toString(), "generated", "src", "main", "java", tel.getProgramName() + ".java");
+        try {
+            Files.createDirectories(dirPath);
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+        return Path.of(dirPath.toString(), tel.getProgramName() + ".java");
+    }
+
+    private static TelProgram readTel(Path inputPath) {
         TelProgram tel;
         try {
-            Path path = FileSystems.getDefault().getPath(args[0]);
-            byte[] proto = Files.readAllBytes(path);
+            byte[] proto = Files.readAllBytes(inputPath);
             tel = TelProgram.parseFrom(proto);
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
         return tel;
+    }
+
+    private static void compileToJava(TelProgram tel, Path outputPath) {
+
     }
 }

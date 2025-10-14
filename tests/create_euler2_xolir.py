ddca273605e8365fb121b76641d66b2fb4b5eccf
Mark <mverleg.noreply@gmail.com>
2025-10-14 23:09:31 +0200

    Update python test, thanks Junie

--- tests/create_euler2_xolir.py
+++ tests/create_euler2_xolir.py
@@ -5,20 +5,17 @@ import sys
 
 from xolir.builtin_type_pb2 import BuiltinType
 from xolir.builtin_function_pb2 import BuiltinFunc
-from xolir.refs_pb2 import TypeRef
-from xolir.type_pb2 import TypedName
-from xolir.struct_pb2 import Struct
-from xolir.function_pb2 import Function
+from xolir.refs_pb2 import TypeRef, TypedName, FuncRef
+from xolir.type_pb2 import TypeDef
+from xolir.type_struct_pb2 import StructType
+from xolir.function_pb2 import Function, FunctionType
 from xolir.expression_pb2 import Expression, Read, Store, Call, If, While, Return
 from xolir.debug_pb2 import SourceFile
-from xolir.tel_pb2 import TelProgram
+from xolir.program_pb2 import Program
 
-prog = TelProgram(
+prog = Program(
     program_name='euler2',
-    ir_version=1,
-    telc_version=1,
-    sources=[SourceFile(
-        id=0,
+    files=[SourceFile(
         name='test',
         source='''
 def even_fib_sub(max):
@@ -33,41 +30,44 @@ def even_fib_sub(max):
     return sum
 ''',
     )],
-    structs=[
-        Struct(
-            id=0,
-            name="Point",
-            fields=[
-                TypedName(name="x", typ=TypeRef(builtin=BuiltinType.REAL_64)),
-                TypedName(name="y", typ=TypeRef(builtin=BuiltinType.REAL_64)),
-            ],
+    types=[
+        TypeDef(
+            struct=StructType(
+                name="Point",
+                fields=[
+                    TypedName(name="x", type_id=TypeRef(builtin=BuiltinType.REAL_64)),
+                    TypedName(name="y", type_id=TypeRef(builtin=BuiltinType.REAL_64)),
+                ],
+            ),
         ),
     ],
-    functions=[
+    funcs=[
         Function(
-            id=0,
             name="main",
-            args=[],
-            results=[TypeRef()],
+            typ=FunctionType(
+                args=[],
+                results=[],
+            ),
             code=[
-                Expression(call=Call(func_ix=1, arguments=[Expression(int=4_000_000)])),
+                Expression(call=Call(func=FuncRef(func_ix=1), arguments=[Expression(int=4_000_000)])),
                 Expression(return_=Return(value=Expression(int=0))),
             ],
         ),
         Function(
-            id=0,
             name="even_fib_sub",
-            args=[
-                TypedName(name="max", typ=TypeRef(builtin=BuiltinType.S_INT_64)),  # 0
-            ],
-            results=[
-                TypeRef(builtin=BuiltinType.S_INT_64)
-            ],
+            typ=FunctionType(
+                args=[
+                    TypedName(name="max", type_id=TypeRef(builtin=BuiltinType.S_INT_64)),  # 0
+                ],
+                results=[
+                    TypeRef(builtin=BuiltinType.S_INT_64)
+                ],
+            ),
             locals=[
-                TypedName(name="sum", typ=TypeRef(builtin=BuiltinType.S_INT_64)),  # 1
-                TypedName(name="first", typ=TypeRef(builtin=BuiltinType.S_INT_64)),  # 2
-                TypedName(name="second", typ=TypeRef(builtin=BuiltinType.S_INT_64)),  # 3
-                TypedName(name="new", typ=TypeRef(builtin=BuiltinType.S_INT_64)),  # 4
+                TypedName(name="sum", type_id=TypeRef(builtin=BuiltinType.S_INT_64)),  # 1
+                TypedName(name="first", type_id=TypeRef(builtin=BuiltinType.S_INT_64)),  # 2
+                TypedName(name="second", type_id=TypeRef(builtin=BuiltinType.S_INT_64)),  # 3
+                TypedName(name="new", type_id=TypeRef(builtin=BuiltinType.S_INT_64)),  # 4
             ],
             code=[
                 Expression(store=Store(var_ix=1, value=Expression(int=0))),
@@ -75,20 +75,20 @@ def even_fib_sub(max):
                 Expression(store=Store(var_ix=3, value=Expression(int=1))),
                 Expression(
                     while_=While(
-                        condition=Expression(call=Call(builtin=BuiltinFunc.LT_S64, arguments=[
+                        condition=Expression(call=Call(func=FuncRef(builtin=BuiltinFunc.LT_S64), arguments=[
                             Expression(read=Read(var_ix=3)), Expression(read=Read(var_ix=0))])),
                         code=[
                             Expression(store=Store(var_ix=4,
-                                value=Expression(call=Call(builtin=BuiltinFunc.ADD_S64, arguments=[
+                                value=Expression(call=Call(func=FuncRef(builtin=BuiltinFunc.ADD_S64), arguments=[
                                     Expression(read=Read(var_ix=2)), Expression(read=Read(var_ix=3))])))),
                             Expression(if_=If(
-                                condition=Expression(call=Call(builtin=BuiltinFunc.EQ_S64, arguments=[
-                                    Expression(call=Call(builtin=BuiltinFunc.MOD_S64, arguments=[
+                                condition=Expression(call=Call(func=FuncRef(builtin=BuiltinFunc.EQ_S64), arguments=[
+                                    Expression(call=Call(func=FuncRef(builtin=BuiltinFunc.MOD_S64), arguments=[
                                         Expression(read=Read(var_ix=4)), Expression(int=2)])),
                                     Expression(int=0)])),
                                 code=[
                                     Expression(store=Store(var_ix=1,
-                                        value=Expression(call=Call(builtin=BuiltinFunc.ADD_S64, arguments=[
+                                        value=Expression(call=Call(func=FuncRef(builtin=BuiltinFunc.ADD_S64), arguments=[
                                             Expression(read=Read(var_ix=1)), Expression(read=Read(var_ix=4))])))),
                                 ])),
                             Expression(store=Store(var_ix=2, value=Expression(read=Read(var_ix=3)))),
